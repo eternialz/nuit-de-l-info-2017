291e8f4a4b4c7fa0c97bd22bc71c11297cabaf21
robin4002 <robin4002@gmail.com>
2017-12-08 06:45:28 +0100

    remove help as the doc is in .md file

--- routes/index.js
+++ routes/index.js
@@ -8,44 +8,6 @@ router.get('/', function (req, res) {
     res.send('Welcome to cordon blue api!')
 })
 
-router.get('/help', function (req, res) {
-
-    return res.json({
-    "/users/register":{
-        "type": "post",
-        "help": "Register user",
-        "data_format": {
-            "name": "string",
-            "email": "string",
-            "password": "string"
-        }},
-    "/users/auth" : {
-        "type": "post",
-        "help": "Authentificate user",
-        "data_format": {
-            "email": "string",
-            "password": "string"
-        }},
-    "/reports/create" : {
-        "type": "post",
-        "help": "Create one report",
-        "data_format": {
-            "report_type": "int/string"
-        }},
-    "/reports/nearby" : {
-        "type": "post",
-        "help": "Get Nearby Reports",
-        "data_format": {
-            "latitude": "float",
-            "longitude": "float"
-        }},
-    "/reports/types" : {
-        "type": "post",
-        "help": "Get report type",
-        "data_format": {}}
-    }
-    })})
-
 router.get('/testdb', function (req, res) {
     db.query('SELECT * FROM test', [], (err, result) => {
         if(err) {
