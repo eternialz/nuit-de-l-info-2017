d382c2a3afe71d3e7d7ecb0290ddd4b2ceebd16e
robin4002 <robin4002@gmail.com>
2017-12-08 05:03:13 +0100

    add route to get all reports types

--- routes/reports.js
+++ routes/reports.js
@@ -10,6 +10,44 @@ function _assertNotNull(data){
   return true
 }
 
+router.get('/nearby', function(req, res){
+    // Renvoie les reports existante dans un rayon de 20KM
+    var longitude = req.query.longitude;
+    var latitude = req.query.latitude;
+    const earth_radius = 6371;
+    const max_distance = 20;
+    // http://www.movable-type.co.uk/scripts/latlong.html
+    db.query('SELECT * from reports WHERE $1*acos(cos(pi()*$2/180))*cos(pi()*latitude/180)*cos(pi()(longitude-$3)/180) + sin(pi()*$2/180)*sin(pi()*latitude/180)) < $4',
+        [earth_radius, latitude, longitude, max_distance], (err, result) => {
+            if (err) {
+                return res.status(500).json({
+                    success: false,
+                    error: "SQL Error : " + err.stack
+                })
+            } else {
+                return res.json({
+                    success: true,
+                    result: result
+                })
+            }
+    });
+})
+
+router.get('/types', (req, res) => {
+    db.query('SELECT name, avgduration FROM reports_types', [], (err, result) => {
+        if(err) {
+            return res.status(500).json({
+                success: false,
+                error: "SQL Error : " + err.stack
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            reportstypes: result.rows
+        })
+    })
+})
+
 router.use(utils.isLogin)
 
 router.post('/save', function(req, res){
@@ -36,29 +74,6 @@ router.post('/save', function(req, res){
           success: true
         })
     })
-})
-
-router.get('/nearby', function(req, res){
-    // Renvoie les reports existante dans un rayon de 20KM
-    var longitude = req.query.longitude;
-    var latitude = req.query.latitude;
-    const earth_radius = 6371;
-    const max_distance = 20;
-    // http://www.movable-type.co.uk/scripts/latlong.html
-    db.query('SELECT * from reports WHERE $1*acos(cos(pi()*$2/180))*cos(pi()*latitude/180)*cos(pi()(longitude-$3)/180) + sin(pi()*$2/180)*sin(pi()*latitude/180)) < $4',
-        [earth_radius, latitude, longitude, max_distance], (err, result) => {
-            if (err) {
-                return res.status(500).json({
-                    success: false,
-                    error: "SQL Error : " + err.stack
-                })
-            } else {
-                return res.json({
-                    success: true,
-                    result: result
-                })
-            }
-    });
 })
 
 module.exports = router;
